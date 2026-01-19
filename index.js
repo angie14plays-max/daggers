b1e5ff5ed5a4a94df801f11675ba59bebe29a258
angie14plays-max <angie14plays@gmail.com>
2026-01-19 20:16:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
+import Redis from "ioredis";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const redis = new Redis(process.env.REDIS_URL);
+
 app.get("/", (req, res) => {
-  res.send("Moderation API running");
+  res.send("Moderation API with Redis running");
 });
 
 const PORT = process.env.PORT || 3000;
